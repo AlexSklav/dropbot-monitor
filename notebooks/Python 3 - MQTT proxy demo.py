8acc28fd3a20d30ca221ac986359e8b85defcd1d
Christian Fobel <christian@fobel.net>
2019-07-03 15:32:23 -0400

    feat(mqtt-proxy): add Python 3 demo notebook
    Add Python 3 MQTT proxy demo notebook, which demonstrates how to use the
    [Jupyter/IPython async REPL][1] to call async code.
    
    [1]: https://blog.jupyter.org/ipython-7-0-async-repl-a35ce050f7f7

---
+++ notebooks/Python 3 - MQTT proxy demo.py
@@ -0,0 +1,49 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+# Workaround https://github.com/jupyter/notebook/issues/3397
+import nest_asyncio
+nest_asyncio.apply()
+
+import sys
+print(sys.executable)
+print(sys.version)
+
+import logging
+
+import dropbot as db
+import dropbot_monitor as dbm
+import dropbot_monitor.mqtt_proxy
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('dropbot_monitor').setLevel(logging.INFO)
+
+# +
+# Create proxy instance with **asynchronous** API.
+aproxy = dbm.mqtt_proxy.MqttProxy.from_uri(db.proxy_py2.SerialProxy, 'dropbot', 'localhost',
+                                           async_=True)
+# Create proxy instance with **synchronous** API.
+# Reuse MQTT client connection from `aproxy`.
+proxy = dbm.mqtt_proxy.MqttProxy(db.proxy_py2.SerialProxy, client=aproxy.__client__,
+                                 async_=False)
+
+proxy.voltage = 100  # Using sync API
+display(proxy.voltage)  # Using sync API
+display(await aproxy.voltage)  # Using async API
+proxy.voltage = 50  # Using sync API
+display(proxy.voltage)  # Using sync API
+display(await aproxy.voltage)  # Using async API
+proxy.properties
